d7ccc0960a7bcbad308adedd4f5fc7ec332ff282
Денис Ляне <denislyane@yandex.ru>
2025-02-14 22:48:31 +0300

    первая отправка7

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -13,11 +13,6 @@ DEBUG = os.getenv('DEBUG', False) == 'True'
 
 ALLOWED_HOSTS = os.getenv('HOSTS', 'localhost').split(',')
 
-CSRF_TRUSTED_ORIGINS = [
-    'http://foodgramlyane.zapto.org/',
-    'https://www.foodgramlyane.zapto.org',
-]
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
