f053b2de5a1b993f6adb9c14de32b5f1e11e891c
Денис Ляне <denislyane@yandex.ru>
2025-02-15 23:10:48 +0300

    первая отправка8

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -13,6 +13,11 @@ DEBUG = os.getenv('DEBUG', False) == 'True'
 
 ALLOWED_HOSTS = os.getenv('HOSTS', 'localhost').split(',')
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://foodgrambyplahosha.ddns.net',
+    'https://www.foodgrambyplahosha.ddns.net',
+]
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
