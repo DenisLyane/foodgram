a56721d96244093075b939130ebb21fdb4c1774a
Денис Ляне <denislyane@yandex.ru>
2025-02-18 01:09:02 +0300

    первая отправка10

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -61,7 +61,7 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.getenv('POSTGRES_DB', 'django'),
         'USER': os.getenv('POSTGRES_USER', 'django_user'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
