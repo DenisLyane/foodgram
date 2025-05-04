14f1fecf9a1495565bd7f655706fc369f18a18a7
Денис Ляне <denislyane@yandex.ru>
2025-05-04 21:59:12 +0300

    первая отправка

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -146,17 +146,11 @@ class Recipe(models.Model):
         verbose_name_plural = 'Рецепты'
 
     def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
-        if not self.short_link:
-            self.short_link = (
-                f'https://foodgramdlyane.zapto.org/r/{self.pk}/'
-            )
-            self.save(update_fields=['short_link'])
-        if not self.full_link:
-            self.full_link = (
-                f'https://foodgramdlyane.zapto.org/recipes/{self.pk}/'
-            )
-            self.save(update_fields=['full_link'])
+    if not self.short_link:
+        self.short_link = f'https://foodgramdlyane.zapto.org/r/{self.pk}/'
+    if not self.full_link:
+        self.full_link = f'https://foodgramdlyane.zapto.org/recipes/{self.pk}/'
+    super().save(*args, **kwargs)
 
     def __str__(self):
         return self.name
