d86f67401ff72e5100d39fcd2406e48d03750d83
Денис Ляне <denislyane@yandex.ru>
2025-05-04 23:46:33 +0300

    первая отправка

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,17 +1,38 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.db.models import UniqueConstraint
+from django.db.models import CheckConstraint, F, Q, UniqueConstraint
+
+from users.constants import EMEIL_LENGTH, NAME_LENGTH
 
 
 class User(AbstractUser):
 
-    first_name = models.CharField('Имя', max_length=150, blank=False)
-    last_name = models.CharField('Фамилия', max_length=150, blank=False)
+    first_name = models.CharField(
+        'Имя',
+        max_length=NAME_LENGTH,
+        blank=False
+    )
+    last_name = models.CharField(
+        'Фамилия',
+        max_length=NAME_LENGTH,
+        blank=False
+    )
     avatar = models.ImageField(
-        blank=True, null=True, upload_to='users/avatar/')
-    email = models.EmailField('Адрес эл.почты', max_length=254, unique=True)
+        blank=True,
+        null=True,
+        upload_to='users/avatar/'
+    )
+    email = models.EmailField(
+        'Адрес эл.почты',
+        max_length=EMEIL_LENGTH,
+        unique=True
+    )
     username = models.CharField(
-        'Логин', max_length=150, unique=True, blank=False)
+        'Логин',
+        max_length=NAME_LENGTH,
+        unique=True,
+        blank=False
+    )
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = (
@@ -24,23 +45,31 @@ class User(AbstractUser):
     class Meta:
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
-        ordering = ['id']
+        ordering = ('id')
 
     def __str__(self):
         return self.username
 
 
 class Subscription(models.Model):
-    user = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='subscriber')
+
     subscribing = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='subscribing')
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='subscriber')
 
     class Meta:
-        verbose_name = 'Подписки'
-        verbose_name_plural = 'Подписки'
-        ordering = ('id',)
         constraints = [
             UniqueConstraint(
-                fields=['user', 'subscribing'], name='unique_together')
+                fields=['user', 'subscribing'], name='unique_together'
+            ),
+            CheckConstraint(
+                check=~Q(user=F('subscribing')),
+                name='chek_self_subscription'
+            )
         ]
+        verbose_name = 'Подписки'
+        verbose_name_plural = 'Подписки'
+
+    def __str__(self):
+        return f'{self.user} подписан на {self.subscribing}'
